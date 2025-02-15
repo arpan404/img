dac419c71dfa84905bf3c0b6d3c6f9b42a685eca
Arpan Bhandari <itsmearpan1@gmail.com>
2025-02-15 14:46:40 -0600

    chore: normal comments added

--- img/config.py
+++ img/config.py
@@ -7,15 +7,18 @@ class Config:
 
     def __int__(self, path: str = os.path.join(os.getcwd(), 'config.json')):
         self.path = path
-
-    # load the config file from the path and return the config data
-    # donot load the config file again if it is already loaded
+        
     def load(self):
+        """
+        Loads the configuration file and return the save and return the config data
+        """
         if self.config_data is None:
             with open(self.path, 'r') as file:
                 self.config_data = json.load(file)
         return self.config_data
 
-    # style is what defines the style of the content, including the voice, prompt, and other parameters
     def style(self, variant: str) -> dict:
+        """
+        Returns the style of the content, which indicates the style of the content, including the voice, prompt, and other parameters
+        """
         return self.config_data[variant]
