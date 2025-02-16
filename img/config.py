0c9acf9f971d2270588c4a9e90bf1234ac2858df
Arpan Bhandari <arpanbhandari@Arpans-MacBook-Air.local>
2025-02-16 01:56:19 -0600

    feat: audio file generates

--- img/config.py
+++ img/config.py
@@ -5,9 +5,9 @@ import os
 class Config:
     config_data = None
 
-    def __init__(self, path: str = os.path.join(os.getcwd(), 'config.json')):
+    def __init__(self, path: str = os.path.join(os.getcwd(), "config.json")):
         # set the absolute path of the config file
-        if (not os.path.isabs(path)):
+        if not os.path.isabs(path):
             path = os.path.join(os.getcwd(), path)
         if not os.path.exists(path):
             raise FileNotFoundError(f"Configuration file not found: {path}")
@@ -18,7 +18,7 @@ class Config:
         Loads the configuration file and return the save and return the config data
         """
         if self.config_data is None:
-            with open(self.path, 'r') as file:
+            with open(self.path, "r") as file:
                 self.config_data = json.load(file)
         return self.config_data
 
--- img/content.py
+++ img/content.py
@@ -1,7 +1,6 @@
 import os
 from google import genai
 import uuid
-import pyttsx3
 import re
 from gtts import gTTS
 from pydub import AudioSegment
@@ -16,16 +15,17 @@ class Content:
             if not os.path.exists(os.path.join(os.getcwd(), "temp")):
                 os.mkdir(os.path.join(os.getcwd(), "temp"))
 
-            audio_path = os.path.join(
-                os.getcwd(), "temp", f'{self.__content_id}.mp3')
-            tts = gTTS(text=text, lang='en', slow=False)
+            audio_path = os.path.join(os.getcwd(), "temp", f"{self.__content_id}.mp3")
+            tts = gTTS(text=text, lang="en")
             tts.save(audio_path)
             return audio_path
         except Exception as e:
             print(e)
             return None
 
-    def __speed_up_audio(self, input_path: str, output_path: str, speed: float = 1.5) -> bool:
+    def __speed_up_audio(
+        self, input_path: str, output_path: str, speed: float = 1.5
+    ) -> bool:
         """
         Speeds up the audio file using pydub.
         """
@@ -33,12 +33,12 @@ class Content:
             audio = AudioSegment.from_file(input_path)
             sped_up_audio = audio.speedup(playback_speed=speed)
             sped_up_audio.export(output_path, format="mp3")
-            return True
+            return sped_up_audio.duration_seconds
         except Exception as e:
             print(e)
             return False
 
-    def __generate_video(self, prompt: str) -> str | None:
+    def __generate_video(self):
         return None
 
     def __generate_story(self, prompt: str):
@@ -49,10 +49,9 @@ class Content:
             api_key = os.getenv("GEMINI_API_KEY")
             client = genai.Client(api_key=api_key)
             response = client.models.generate_content(
-                model='gemini-2.0-flash-001', contents=prompt
+                model="gemini-2.0-flash-001", contents=prompt
             )
-            print(response.text)
-            self.__story = re.sub(r'\(.*?\)', '', response.text)
+            self.__story = re.sub(r"\(.*?\)", "", response.text)
             return response.text
 
         except Exception as e:
@@ -61,17 +60,19 @@ class Content:
     def generate(self, style: dict) -> str | None:
         self.__content_id = str(uuid.uuid4())
         self.__style = style
-        self.__audio__speed = 1.2
-        self.__generate_story(style['prompt'])
-        print(self.__story)
+        self.__audio__speed = 1.25
+        self.__generate_story(style["prompt"])
         generated_audio_path = self.__generate_audio(self.__story)
         if generated_audio_path is None:
             print("Error generating audio")
             raise Exception("Error generating audio")
         sped_up_audio_path = os.path.join(
-            os.getcwd(), "temp", f'{self.__content_id}_sped_up.mp3')
-
-        if not self.__speed_up_audio(generated_audio_path, sped_up_audio_path, self.__audio__speed):
+            os.getcwd(), "temp", f"{self.__content_id}_sped_up.mp3"
+        )
+        self.__audio_duration = self.__speed_up_audio(
+            generated_audio_path, sped_up_audio_path
+        )
+        if self.__audio_duration is False:
             print("Error speeding up audio")
             raise Exception("Error speeding up audio")
-        pass
+        self.__generate_video()
