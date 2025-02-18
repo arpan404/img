0104d22ab618a7b7f9cb7686c222fe1088ea5fc5
arpan404 <itsmearpan1@gmail.com>
2025-02-18 00:19:36 -0600

    refactor: making img class, added generate_story

--- img/img.py
+++ img/img.py
@@ -5,13 +5,14 @@ import re
 from moviepy import VideoFileClip, AudioFileClip
 import random
 
+
 class Img:
 
     def __init__(self, content_style, content_id):
         """
         content_style: dict
         content_id: str
-        
+
         content_id is the id of the content to be generated.
         content_style is the style of the content to be generated, defined in config.json.
         """
@@ -20,21 +21,24 @@ class Img:
 
         if not self.__content_style:
             raise Exception("No content style specified")
-        
+
         if not self.__content_style["prompt"]:
             raise Exception("No prompt specified")
 
         self.__validateVideoPath()
-       
-    
+
     def __validateVideoPath(self):
         """
+
         Validates the video path specified in the content style.
+        Will check the value inside self.__content_style["videoFile"] and convert it to an absolute path if it is not already.
+
+        Will raise an exception if the video file does not exist.
         """
 
         if not self.__content_style["videoFile"]:
             raise Exception("No video file specified")
-        
+
         is_abs = os.path.isabs(self.__content_style["videoFile"])
 
         # check if path is absolute or relative and convert to absolute path
@@ -43,7 +47,9 @@ class Img:
         else:
             is_relative = os.path.isfile(self.__content_style["videoFile"])
             if is_relative:
-                video_path = os.path.join(os.getcwd(), self.__content_style["videoFile"])
+                video_path = os.path.join(
+                    os.getcwd(), self.__content_style["videoFile"]
+                )
             else:
                 video_path = os.path.join(
                     os.getcwd(), "videos", self.__content_style["videoFile"]
@@ -51,12 +57,16 @@ class Img:
         # check if path exists and is a file
         if not os.path.exists(video_path) or not os.path.isfile(video_path):
             raise Exception("Video file does not exist")
-        
+
         # if path is valid, set it in the content style
         self.__content_style["videoFile"] = video_path
 
+    def __check_create_temp_dir(self):
+        if not os.path.exists(os.path.join(os.getcwd(), "temp")):
+            os.makedirs(os.path.join(os.getcwd(), "temp"))
+
     def __generate_video(self):
-    
+
         video_path = self.__content_style["videoFile"]
 
         video = VideoFileClip(video_path)
@@ -71,9 +81,9 @@ class Img:
         # crop video to 16:9 aspect ratio (for short content)
         content_width = int(video_height * 9 / 16)
 
-        if content_width> video_width:
+        if content_width > video_width:
             raise Exception("Video is not wide enough")
-        
+
         # pixels needed to be cropped from left side
         crop_x1 = (video_width - content_width) / 2
 
@@ -84,10 +94,40 @@ class Img:
         video_starting_point = random.uniform(0, video.duration - audio.duration)
 
         #  trim the video, then crop it, then add the audio (in this order for performance reasons)
-        edited_clip = video.subclipped(
-            video_starting_point, video_starting_point + self.__audio_duration
-        ).without_audio().cropped(x1=crop_x1, x2=crop_x2).with_audio(audio)
+        edited_clip = (
+            video.subclipped(
+                video_starting_point, video_starting_point + self.__audio_duration
+            )
+            .without_audio()
+            .cropped(x1=crop_x1, x2=crop_x2)
+            .with_audio(audio)
+        )
 
-        self.__edited_video_path = os.path.join(os.getcwd(), "temp", f"{self.__content_id}.mp4")
+        self.__check_create_temp_dir()
 
-        edited_clip.write_videofile(self.__edited_video_path, codec="libx264", audio_codec="aac")
+        # path to save the edited video
+        self.__edited_video_path = os.path.join(
+            os.getcwd(), "temp", f"{self.__content_id}.mp4"
+        )
+
+        edited_clip.write_videofile(
+            self.__edited_video_path, codec="libx264", audio_codec="aac"
+        )
+
+    def __generate_story(self) -> None:
+        """
+
+        Generate a story based on the prompt specified in the content style, and set it to self.__story.
+        Will use google Gemini 2.0 flash as LLM
+
+        Will raise an exception if the story generation fails.
+
+        """
+        api_key = os.getenv("GEMINI_API_KEY")
+        client = genai.Client(api_key=api_key)
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001", contents=self.__content_style["prompt"]
+        )
+
+        # remove any text inside parentheses from the story (usually it contains the environment description, which is not needed)
+        self.__story = re.sub(r"\(.*?\)", "", response.text)
